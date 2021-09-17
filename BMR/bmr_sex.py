8e363e5cd9fa4642b29dc07fa632d440fad03755
Grynovetskyy Vladyslav <grynovetskyy@gmail.com>
2021-09-17 09:35:42 +0300

    modify bmr_sex.py

--- BMR/bmr_sex.py
+++ BMR/bmr_sex.py
@@ -1,4 +1,4 @@
-(weight, height, age), sex = [float(input(f"Enter your {rq}: ")) for rq in
+[weight, height, age], sex = [float(input(f"Enter your {rq}: ")) for rq in
                               ["weight (kg)", "height (cm)", "age (years)"]], input("Enter your sex (male|female): ")
 print(f"Your BMR: {88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)}\n") if sex == "male" \
     else print(f"Your BMR: {447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)}") if sex == "female" \
---
+++ Bacterial_growth/report.md
@@ -0,0 +1,7 @@
+# Bacterial_growth
+
+**Task [HERE](task.md)**
+
+**Solution for "Cyanobacteria" [HERE](bg_cyanobacteria.py)**
+
+**Solution for "Pseudomonas Natriegens" [HERE](bg_pseudomonas_natriegens.py)**
