afca3080cfd2d34aeee46625f0a7b33371deca4a
Grynovetskyy Vladyslav <grynovetskyy@gmail.com>
2021-09-30 23:38:49 +0300

    add new solution

--- 10Khours/10k_years.py
+++ 10Khours/10k_years.py
@@ -1,3 +1,14 @@
 import datetime
+import time
 
-print(datetime.datetime.now())
+# 10000*60*60
+seconds = 36000000
+
+time_needed = seconds
+
+end = datetime.datetime.fromtimestamp(round(time.time()+time_needed))
+
+years_needed = end.timestamp() - datetime.datetime.now().timestamp()
+
+print(f"End at (if you`ll start now): {end} about {} years")
+
